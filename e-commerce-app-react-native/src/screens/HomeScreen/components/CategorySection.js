f90eb63b81f4ecc024ac5fc405e239f7551aa52a
vongocman <45393145+vongocman@users.noreply.github.com>
2021-09-03 09:42:33 +0700

    Revert "market"

--- e-commerce-app-react-native/src/screens/HomeScreen/components/CategorySection.js
+++ e-commerce-app-react-native/src/screens/HomeScreen/components/CategorySection.js
@@ -5,7 +5,6 @@ import {
   TouchableOpacity,
   FlatList,
   Image,
-  Button,
 } from "react-native";
 import { ProductItem } from "./ProductItem";
 import CustomText from "../../../components/UI/CustomText";
@@ -35,27 +34,7 @@ export class CategorySection extends React.PureComponent {
         <Image style={styles.background} source={bg} blurRadius={10} />
         <View style={styles.titleHeader}>
           <CustomText style={styles.title}>{name}</CustomText>
-          
         </View>
-        <View style={styles.container}>
-          
-          <TouchableOpacity style={styles.button}>
-            <CustomText style={styles.title}>Gia dụng</CustomText>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.button}>
-            <CustomText style={styles.title}>Thực phẩm</CustomText>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <CustomText style={styles.title}>Bỉm sữa</CustomText>
-          </TouchableOpacity>
-
-          
-          
-          
-        </View>
-        
-        
         <View style={styles.productList}>
           <FlatList
             data={getItems()}
@@ -93,7 +72,7 @@ CategorySection.propTypes = {
 
 const styles = StyleSheet.create({
   category: {
-   // height: 800,
+    height: 518,
     marginHorizontal: 5,
     marginVertical: 5,
     paddingVertical: 15,
@@ -110,20 +89,14 @@ const styles = StyleSheet.create({
   },
   titleHeader: {
     marginHorizontal: 10,
+    marginBottom: 5,
     alignItems: "center",
     justifyContent: "center",
-    padding:3,
-    borderColor:"red",
-    borderWidth:2,
-    borderRadius:10
-   
   },
   title: {
     fontSize: 18,
     color: Colors.red,
     fontWeight: "500",
-   
-   
   },
   list: {
     justifyContent: "space-between",
@@ -145,20 +118,4 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: Colors.red,
   },
-  container: {
-    flexDirection:'row',
-    marginTop: 20,
-    marginLeft: 10,
-  },
-  button:{
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderRadius: 4,
-    backgroundColor: "oldlace",
-    alignSelf: "flex-start",
-    marginHorizontal: "1%",
-    marginBottom: 6,
-    minWidth: "20%",
-    textAlign: "center",
-  }
 });
